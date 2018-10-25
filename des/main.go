a518e81f7d1be606ef0de7785110bd07dbe89adf
Miguel Chan <vvchan@outlook.com>
2018-10-25 11:41:43 +0800

    improved

--- des/main.go
+++ des/main.go
@@ -11,28 +11,48 @@ import (
 
 func main() {
 	var filename, keyStr string
+	silentMode := false
 	encryptMode := true
-	errStr := fmt.Sprintf("Usage: %v {encrypt|decrypt} [filename]", os.Args[0])
+	errStr := fmt.Sprintf("Usage: %v {encrypt|decrypt} [-s|--silent] [{-i|--input} filename]", os.Args[0])
 
 	if len(os.Args) == 1 {
 		fmt.Fprintln(os.Stderr, errStr)
 		os.Exit(1)
 	} else {
-		switch strings.ToLower(os.Args[1]) {
-		case "-h", "--help", "help":
-			fmt.Fprintln(os.Stderr, errStr)
-			os.Exit(0)
-		case "encrypt", "e":
-			encryptMode = true
-		case "decrypt", "d":
-			encryptMode = false
-		default:
-			fmt.Fprintf(os.Stderr, "Unknown argument: %v\n", os.Args[1])
-			fmt.Fprintln(os.Stderr, errStr)
-			os.Exit(1)
+		for i := 1; i < len(os.Args); i++ {
+			switch strings.ToLower(os.Args[i]) {
+			case "-h", "--help", "help":
+				fmt.Fprintln(os.Stderr, errStr)
+				os.Exit(0)
+			case "encrypt", "e":
+				encryptMode = true
+			case "decrypt", "d":
+				encryptMode = false
+			case "-s", "--silent":
+				silentMode = true
+			case "-i", "--input":
+				i++
+				if i == len(os.Args) {
+					fmt.Fprintf(os.Stderr, "Expecting filename after %v\n", os.Args[i-1])
+					fmt.Fprintln(os.Stderr, errStr)
+					os.Exit(1)
+				}
+				filename = os.Args[i]
+			default:
+				fmt.Fprintf(os.Stderr, "Unknown argument: %v\n", os.Args[i])
+				fmt.Fprintln(os.Stderr, errStr)
+				os.Exit(1)
+			}
 		}
-		if len(os.Args) > 2 {
-			filename = os.Args[2]
+	}
+
+	if encryptMode {
+		fmt.Println("***Encryption Mode***")
+	} else {
+		fmt.Println("***Decryption Mode***")
+		if filename == "" {
+			fmt.Println("You must specify a file for decryption!")
+			os.Exit(1)
 		}
 	}
 
@@ -87,23 +107,32 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("***Encrypted/Decrypted Data are Shown Below***")
+	if !silentMode {
+		fmt.Println("***Encrypted/Decrypted Data are Shown Below***")
+	}
 	if encryptMode {
-		fmt.Printf("%v\n", output)
+		if !silentMode {
+			fmt.Printf("%v\n", output)
+		}
+
 		outFile, err := os.Create("EncryptedData")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error creating file: %v", err)
 			os.Exit(1)
 		}
 		outFile.Write(output)
+		println("Output has been saved to EncryptedData")
 	} else {
-		fmt.Printf("%v\n", string(output))
+		if !silentMode {
+			fmt.Printf("%v\n", string(output))
+		}
 		outFile, err := os.Create("DecryptedData.txt")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error creating file: %v", err)
 			os.Exit(1)
 		}
 		outFile.Write(output)
+		println("Output has been saved to DecryptedData.txt")
 	}
 
 }
